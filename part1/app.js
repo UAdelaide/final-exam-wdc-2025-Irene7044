3c77a36595d23f14cc2d5da24eb28e305518a172
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 02:32:56 +0000

    [automatic commit] 2025-06-20 02:32:56 UTC 14.201.221.196

--- part1/app.js
+++ part1/app.js
@@ -54,7 +54,7 @@ app.get('/api/dogs', async (req, res) => {
 app.get('/api/walkrequests/open', async (req, res) => {
     try {
         const [requests] = await db.execute(
-            "SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username AS owner_name FROM WalkRequests wr JOIN Dogs d ON wr.dog_id = d.dog_id JOIN Users u ON d.owner_id = u.user_id WHERE wr.status = 'open';"
+            "SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username AS owner_username FROM WalkRequests wr JOIN Dogs d ON wr.dog_id = d.dog_id JOIN Users u ON d.owner_id = u.user_id WHERE wr.status = 'open';"
         );
         res.json(requests);
     } catch (err) {
