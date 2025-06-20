4c9ce80a625de33273f06f7aab76570c23109ad6
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 02:29:05 +0000

    [automatic commit] 2025-06-20 02:29:04 UTC 14.201.221.196

--- part1/app.js
+++ part1/app.js
@@ -69,7 +69,7 @@ app.get('/api/walkers/summary', async (req, res) => {
             COUNT(DISTINCT wrt.rating_id) AS total_ratings,
             ROUND(AVG(wrt.rating), 2) AS average_rating,
             COUNT (DISTINCT CASE WHEN wrq.status = 'completed' THEN wrq.request_id END) AS completed_walks
-            FROM Users u LEFT JOIN WalkApplications wa ON u.user_id = wa.walker_id LEFT JOIN WalkRequests wrq ON wa.request_id = wrq.request_id
+            FROM Users u LEFT JOIN WalkApplications wa ON u.user_id = wa.walker_id LEFT JOIN WalkRequests wrq ON wa.request_id = wrq.request_id LEFT JOIN WalkRatings wrt ON u.user_id = wrt.walker_id
 
             "
 
