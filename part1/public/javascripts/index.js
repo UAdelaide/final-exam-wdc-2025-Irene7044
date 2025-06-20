aa78719a565a6c2535e3b59606e91dcf4bd746f4
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 03:09:38 +0000

    [automatic commit] 2025-06-20 03:09:37 UTC 14.201.221.196

--- part1/public/javascripts/index.js
+++ part1/public/javascripts/index.js
@@ -5,12 +5,12 @@ createApp({
     return {
       count: ref(0)
     };
-  }
+  },
   methods: {
     fetch_dog_api() {
         fetch('https://dog.ceo/dog-api/documentation/random')
-        .then(res => res.json())
-        .then(data => {
+        .then((res) => res.json())
+        .then((data) => {
             this.dogImage = data.message;
         });
     }
